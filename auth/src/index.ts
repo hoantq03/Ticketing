6954d2dec016932f2be20bcfcda318091c2a2f30
Qhoan_Tran3008 <thaihoang03082003@gmail.com>
2023-08-17 14:22:08 +0700

    [DEV] errors handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,22 @@
-import express from "express";
 import { json } from "body-parser";
+import express from "express";
+
+import { errorHandler } from "./middlewares/error-handler";
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
 
 const app = express();
 app.use(json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("hi there");
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Listening on port 3000!!!!!!!!");
 });
---
+++ auth/src/routes/signout.ts
@@ -0,0 +1,7 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/api/users/signout", (req, res) => {});
+
+export { router as signoutRouter };
