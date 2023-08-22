7138939d367ea1670a8f566589a8fa208d38c909
Qhoan_Tran3008 <thaihoang03082003@gmail.com>
2023-08-22 22:35:12 +0700

    [DEV] client navigation component

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,26 +1,20 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-
-  return <h1>Landing Page</h1>;
+  return currentUser ? (
+    <h1>You're Signed In</h1>
+  ) : (
+    <h1>You're Not Signed In</h1>
+  );
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === "undefined") {
-    const { data } = await axios.get(
-      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-      {
-        headers: req.headers,
-      }
-    );
-
-    return data;
-  } else {
-    const { data } = await axios.get("/api/users/currentuser");
-
-    return data;
-  }
+LandingPage.getInitialProps = async (context) => {
+  const { data } = await buildClient(context)
+    .get("/api/users/currentuser")
+    .catch((err) => {
+      console.log(err);
+    });
+  return data;
 };
 
 export default LandingPage;
