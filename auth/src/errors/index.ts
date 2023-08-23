16ad64906df259a1e1f1da29236767e86a9c9ced
Qhoan_Tran3008 <thaihoang03082003@gmail.com>
2023-08-23 10:47:18 +0700

    [DEV] commmon module

--- auth/src/errors/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./custom-errors";
-export * from "./database-connection-error";
-export * from "./not-found-error";
-export * from "./request-validation-errors";
-export * from "./bad-request";
-export * from "./not-authorized-error";
--- auth/src/middlewares/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./error-handler";
-export * from "./validate-request";
-export * from "./require-auth";
-export * from "./current-user";
