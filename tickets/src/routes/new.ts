fa2a1b377e7cb9d6dfcc0ae2dc0d35637d4711dd
Qhoan_Tran3008 <thaihoang03082003@gmail.com>
2023-08-26 01:09:24 +0700

    [DEV] publish nats created tickets

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,8 @@ import { requireAuth, validateRequest } from "@eztik/common";
 import express, { Response, Request } from "express";
 import { body } from "express-validator";
 import { Ticket } from "../models/ticket";
+import { TicketCreatedPublisher } from "../events/publisher/ticket-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -25,6 +27,12 @@ router.post(
     });
     await ticket.save();
 
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: req.currentUser!.id,
+    });
     res.status(201).send(ticket);
   }
 );
