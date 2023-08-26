dc610e2f16c19e9afd465b4a1e63eae84aacf112
Qhoan_Tran3008 <thaihoang03082003@gmail.com>
2023-08-26 23:32:26 +0700

    [WIP] fix deploy

--- orders/src/routes/cancel-order.ts
+++ orders/src/routes/cancel-order.ts
@@ -1,9 +1,23 @@
 import express, { Request, Response } from "express";
+import { requireAuth, validateRequest } from "@eztik/common";
+import { body } from "express-validator";
+import mongoose from "mongoose";
 
 const router = express.Router();
 
-router.delete("/api/orders::id", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  "/api/orders::id",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("Ticket Id mus be provided"),
+  ],
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as cancelOrderRouter };
