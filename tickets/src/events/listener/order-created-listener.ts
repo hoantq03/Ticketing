30762a5f9c766c04367c4e973a0f7f6ce4593970
Qhoan_Tran3008 <thaihoang03082003@gmail.com>
2023-08-30 11:41:48 +0700

    [DEV] order created listener in ticket module

--- tickets/src/events/listener/order-created-listener.ts
+++ tickets/src/events/listener/order-created-listener.ts
@@ -8,6 +8,8 @@ import {
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
 import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from "../publisher/ticket-updated-publisher";
+import { natsWrapper } from "../../nats-wrapper";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
@@ -27,6 +29,15 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
     //save the ticket
     await ticket.save();
+
+    new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      price: ticket.price,
+      title: ticket.title,
+      userId: ticket.userId,
+      version: ticket.version,
+      orderId: ticket.orderId,
+    });
 
     //ack the message
     msg.ack();
