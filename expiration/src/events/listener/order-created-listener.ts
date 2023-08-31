e887e2c479ba22ab22018111b8459a969f99ca77
Qhoan_Tran3008 <thaihoang03082003@gmail.com>
2023-08-31 16:48:23 +0700

    [DEV] expiration module

---
+++ expiration/src/events/listener/order-created-listener.ts
@@ -0,0 +1,21 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@eztik/common";
+import { queueGroupName } from "./queue-group-name";
+import { Message } from "node-nats-streaming";
+import { expirationQueue } from "../../queues/expiration-queue";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  readonly queueGroupName = queueGroupName;
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log(delay);
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      { delay }
+    );
+
+    msg.ack();
+  }
+}
--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,17 +1,8 @@
-import mongoose from "mongoose";
 import { app } from "./app";
+import { OrderCreatedListener } from "./events/listener/order-created-listener";
 import { natsWrapper } from "./nats-wrapper";
-import { TicketCreatedListener } from "./events/listeners/ticket-created-listener";
-import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listener";
-import { ExpirationCompletedListener } from "./events/listeners/expiration-completed-listener";
 
 const start = async () => {
-  if (!process.env.JWT_KEY) {
-    throw new Error("JWT_KEY must be defined");
-  }
-  if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI must be defined");
-  }
   if (!process.env.NATS_CLIENT_ID) {
     throw new Error("NATS_CLIENT_ID must be defined");
   }
@@ -21,7 +12,6 @@ const start = async () => {
   if (!process.env.NATS_CLUSTER_ID) {
     throw new Error("NATS_CLUSTER_ID must be defined");
   }
-
   try {
     await natsWrapper.connect(
       process.env.NATS_CLUSTER_ID,
@@ -33,19 +23,12 @@ const start = async () => {
       process.exit();
     });
 
+    new OrderCreatedListener(natsWrapper.client).listen();
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
-
-    new TicketCreatedListener(natsWrapper.client).listen();
-    new TicketUpdatedListener(natsWrapper.client).listen();
-    new ExpirationCompletedListener(natsWrapper.client).listen();
-
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected to MongoDb");
   } catch (err) {
     console.error(err);
   }
-
   app.listen(3000, () => {
     console.log("Listening on port 3000!!!!!!!!");
   });
