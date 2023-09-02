678ba6b10bdd9932cdb6f78d1d6c23891b0711e7
Qhoan_Tran3008 <thaihoang03082003@gmail.com>
2023-09-02 14:39:53 +0700

    [DEV] changed mongo db

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -11,7 +11,7 @@ let mongo: any;
 beforeAll(async () => {
   process.env.JWT_KEY = "hoantq3008";
 
-  const mongo = await MongoMemoryServer.create();
+  mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
 
   await mongoose.connect(mongoUri, {});
